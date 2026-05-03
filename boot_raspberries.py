b4ace118e2c049ea043b23007c182979016b070d
pedro tarcizio pereira galdino <devpedrotarcizio@gmail.com>
2026-05-03 20:24:31 -0300

    Boot script added

---
+++ boot_raspberries.py
@@ -0,0 +1,35 @@
+import socket, uuid
+from random import randint
+from dataclasses import dataclass, asdict
+
+@dataclass
+class MachineInfo:
+    hostname: str
+    ipAddress: str
+    ID: int
+    macAddress: str
+
+
+def get_machine_info():
+    hostname = socket.gethostname()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+
+    mac = uuid.getnode()
+    mac_address = ':'.join(f'{(mac >> ele) & 0xff:02x}' for ele in range(40, -1, -8))
+
+    ID = int(mac_address.replace(":", ""), 16)
+
+    machine = MachineInfo(
+        hostname=hostname,
+        ipAddress=ip,
+        ID=ID,
+        macAddress=mac_address
+    )
+
+    return asdict(machine)
+
+print(get_machine_info())
