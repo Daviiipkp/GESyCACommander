05d9842e153ca06ab34f1d24360825fbfcc8c513
pedro tarcizio pereira galdino <devpedrotarcizio@gmail.com>
2026-05-04 09:49:58 -0300

    Debug in startup.py

--- src/main/python/startup.py
+++ src/main/python/startup.py
@@ -36,8 +36,19 @@ data = get_machine_info()
 
 
 # Alterar endpoint para enviar a machine info para o backend
-#response = requests.post(
-#    "http://daviipkp:4500/boot",
-#    json=data
-#)
+try:
+    print("Registing in master...")
+
+    response = requests.post(
+        "http://daviipkp:4500/boot",
+        json=data,
+        timeout=3
+    )
+
+    response.raise_for_status()
+
+    print("Successful register in master!")
+
+except requests.exceptions.RequestException as e:
+    print("Erro ao enviar a requisição: ", e)
 
